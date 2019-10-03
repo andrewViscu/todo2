5829e69428f3e7b9f3373060de03e745a05ce423
Viscu Andrew <samsungmvideo33@gmail.com>
2019-10-03 20:49:14 +0300

    Now you can hide notification when you add/remove a new list + more attractive design

--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -20,13 +20,6 @@
 		li,a,div{
 			font-family: 'Roboto',sans-serif;
 		}
-		a{
-			text-decoration: none;
-			color:#00397a;
-		}
-		a:hover{
-			color:#0668d6;
-		}
 		ul{
 			list-style-type:decimal; 
 		}
@@ -36,6 +29,9 @@
   			margin:0px auto;
   			z-index:0;
  		}
+ 		a{
+			text-decoration: none;
+			color:#000;}
  		.alert-success{
  			background-color:#68bf52;
  			color:#fff;
@@ -44,34 +40,65 @@
  			background-color:#b00c04;
  			color:#fff;
  		}
+ 		.lists{
+ 			background-color: #ededed;
+ 			list-style-position:inside; 
+ 			padding: 0;
+ 			  box-shadow: 1px 2px 4px rgba(0, 0, 0, .5);
+ 		}
+ 		.lists li:hover{
+ 			background-color: #d4d2d2;
+ 		}
+ 		.lists li{
+ 			padding:10px;
+ 			border:1px solid #bfbfbf;
+ 			margin-top: -1px;
+ 			margin-left: -1px;
+ 		}
+ 		.hide_btn{
+ 			position: relative;
+ 			float:right;
+ 			background-color: #595959;
+			color: white;
+			margin-top: -5px;
+			padding: 6px 10px;
+			border: none;
+			border-radius: 4px;
+			cursor: pointer;
+			margin-bottom: 5px;
+ 		}
 	</style>
 
 @if (session('status'))
-<div class="alert alert-success">
+<div class="alert alert-success" id="list_added">
 	{{session('status')}}
+	<button class="hide_btn" onclick="document.getElementById('list_added').style.display = 'none';document.getElementsByClassName('hide_btn').style.display = 'none'">Hide</button>
 </div>
 @elseif(session('status2'))
-<div class="alert alert-danger">
+<div class="alert alert-danger" id="list_removed">
 	{{session('status2')}}
+	<button class="hide_btn" onclick="document.getElementById('list_removed').style.display = 'none';document.getElementsByClassName('hide_btn').style.display = 'none'">Hide</button>
 </div>
 @endif
 <div id="wrapper">
 <h1>All Information About Lists</h1>
-<ul>
+<ul  class="lists">
 @foreach ($my_lists as $list)
 <li> {{ $list}}  </li>
 @endforeach
 </ul>
 <h1>Only Names Of Lists</h1>
-<ul>
+<ul class="lists">
 @foreach ($my_lists as $list)
-<li><a href="/lists/id/{{$list->id}}">{{ $list->name_of_list}}</a></li>
+<a href="/lists/id/{{$list->id}}"><li>{{ $list->name_of_list}}</li></a>
 @endforeach
 </ul>
 <br>
 <button style="background-color: #45a049;padding: 10px 15px;color:#fff;margin-bottom: 5px; border: none;border-radius: 4px;cursor: pointer;" ><a href="/lists/create" style="text-decoration: none;color: white;">Create New List</a></button>
 
 <button style="background-color: #bd2424;padding: 10px 15px;color:#fff; border: none;margin-bottom: 5px;border-radius: 4px;cursor: pointer;" value="Remove List"><a href="/lists/remove" style="text-decoration: none;color: white;">Remove List</a></button>
+
+
 
 </div>
 </body>
